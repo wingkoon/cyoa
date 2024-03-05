a89cd9d9aab28b0bfad7b9b986a29e248e675399
wingkoon <wingkoon@yahoo.com>
2024-03-05 11:18:09 -0800

    photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
-
+import PhotoFavButton from './PhotoFavButton';
 
 const sampleDataForPhotoListItem = {
   id: "1",
@@ -14,9 +14,34 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ toggleFavourite, isFavorited, imageSource, profile, username, location, openModal }) => {
+  return (
+    <li className="photo-list__item">
+      <PhotoFavButton
+        toggleFavourite={toggleFavourite}
+        isFavorited={isFavorited}
+      />
+      <img
+        className="photo-list__image"
+        src={imageSource}
+        alt="User's photo"
+         onClick={openModal}
+      />
+      <section className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={profile}
+          alt="User's profile"
+        />
+        <div className="photo-list__user-info">{username}
+          <div className="photo-list__user-location">{location.city}, 
+          {location.country}</div>
+        </div>
+      </section>
+    </li>
+  );
 };
+
 
 export default PhotoListItem;
 
