1f0dc77772423396db8b2af6f25c88b48d4c5664
wingkoon <wingkoon@yahoo.com>
2024-03-05 18:49:50 -0800

    add homeroute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 import '../styles/HomeRoute.scss';
+import TopNavigation from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
+import useApplicationData from '../hooks/useApplicationData';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+  const { photoData, topicData, fetchPhotosByTopic, toggleFavourite, isFavorited } = useApplicationData();
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation isFavorited={isFavorited} onTopicClick={fetchPhotosByTopic} />
+      <PhotoList
+        photos={photoData}
+        topics={topicData}
+        openModal={props.openModal}
+        isFavorited={isFavorited}
+        toggleFavourite={toggleFavourite} />
+
     </div>
   );
 };
 
 export default HomeRoute;
+
